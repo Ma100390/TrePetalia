6a7e1f2513519bbf335d1d8f4fa9747680060ac2
Manuel <magini15@gmail.com>
2024-12-27 13:04:21 +0100

    configurazioni accessibilità

--- src/app/phome-page/phome-page.component.ts
+++ src/app/phome-page/phome-page.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { CnavbarComponent } from '../cnavbar/cnavbar.component';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { faArrowRight} from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight,faPeopleArrows, faBalanceScale, faLightbulb, faHeart } from '@fortawesome/free-solid-svg-icons';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-phome-page',
   standalone: true,
-  imports: [CnavbarComponent,RouterOutlet,FontAwesomeModule,RouterModule],
+  imports: [FontAwesomeModule,RouterModule],
   templateUrl: './phome-page.component.html',
   styleUrl: './phome-page.component.css'
 })
 export class PhomePageComponent {
   faArrowRight = faArrowRight;
+  faPeopleArrows = faPeopleArrows;
+  faBalanceScale = faBalanceScale;
+  faLightbulb = faLightbulb;
+  faHeart = faHeart;
 }
