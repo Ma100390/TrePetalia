5dfb788a972e0601bcaaafd2059dc041e3cdb068
Manuel <magini15@gmail.com>
2025-01-16 18:44:53 +0100

    Initial commit

--- src/app/phome-page/phome-page.component.ts
+++ src/app/phome-page/phome-page.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
-import { CnavbarComponent } from '../cnavbar/cnavbar.component';
-import { RouterModule, RouterOutlet } from '@angular/router';
-import { faArrowRight,faPeopleArrows, faBalanceScale, faLightbulb, faHeart } from '@fortawesome/free-solid-svg-icons';
+
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { faArrowRight, faPhone, faPeopleArrows, faBalanceScale, faLightbulb, faHeart, faUserGraduate, faHeartbeat, faStethoscope, faBrain, faChalkboardTeacher, faBriefcase } from '@fortawesome/free-solid-svg-icons';
+
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-phome-page',
   standalone: true,
-  imports: [FontAwesomeModule,RouterModule],
+  imports: [FontAwesomeModule, RouterModule],
   templateUrl: './phome-page.component.html',
   styleUrl: './phome-page.component.css'
 })
 export class PhomePageComponent {
+  constructor(private route: ActivatedRoute) { }
+  faUserGraduate = faUserGraduate;
+  faBrain = faBrain;
+  faHeartbeat = faHeartbeat
+  faPhone = faPhone;
+  faBriefcase = faBriefcase;
+  faStethoscope = faStethoscope
+  faChalkboardTeacher = faChalkboardTeacher;
   faArrowRight = faArrowRight;
   faPeopleArrows = faPeopleArrows;
   faBalanceScale = faBalanceScale;
